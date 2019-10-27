86b5c1fabfd8e5d0b7991e3c1e057d3c87c86528
Li.Liu <nox60@hotmail.com>
2019-10-27 22:13:37 +0800

    Update files

--- httpserver/main.go
+++ httpserver/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	. "daily_golang/httpserver/utils"
-	"fmt"
 )
 
 func main() {
 	//defer db.SqlDB.Close()
 	router := InitRouter()
-	router.Run(":8000")
-	fmt.Println("test resored111222")
+	router.Run(":8080")
 }
