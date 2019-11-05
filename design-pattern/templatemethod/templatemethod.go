294b4e6e6a7a76f3d5030ac7510f08889bc7c141
Li.Liu <nox60@hotmail.com>
2019-11-05 23:48:46 +0800

    Add 2nd observer method

--- design-pattern/templatemethod/templatemethod.go
+++ design-pattern/templatemethod/templatemethod.go
@@ -67,3 +67,25 @@ func NewFTPDownloader() Downloader {
 func (d *FTPDownloader) download() {
 	fmt.Printf("download %s via ftp\n", d.uri)
 }
+
+func ExampleHTTPDownloader() {
+	var downloader Downloader = NewHTTPDownloader()
+
+	downloader.Download("http://example.com/abc.zip")
+	// Output:
+	// prepare downloading
+	// download http://example.com/abc.zip via http
+	// http save
+	// finish downloading
+}
+
+func ExampleFTPDownloader() {
+	var downloader Downloader = NewFTPDownloader()
+
+	downloader.Download("ftp://example.com/abc.zip")
+	// Output:
+	// prepare downloading
+	// download ftp://example.com/abc.zip via ftp
+	// default save
+	// finish downloading
+}
