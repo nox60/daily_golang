90cd8561357c7db0f4b0f3a19d47a3f4c3b23989
Li.Liu <nox60@hotmail.com>
2019-11-06 23:22:16 +0800

    Add 2nd observer method

--- design-pattern/visitor/visitor.go
+++ design-pattern/visitor/visitor.go
@@ -15,3 +15,13 @@ type EnterpriseCustomer struct {
 type CustomerCol struct {
 	customers []Customer
 }
+
+func (c *CustomerCol) Add(customer Customer) {
+	c.customers = append(c.customers, customer)
+}
+
+func (c *CustomerCol) Accept(visitor Visitor) {
+	for _, customer := range c.customers {
+		customer.Accept(visitor)
+	}
+}
