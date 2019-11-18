09c1f4e6546d386e940d7361764add5ffca24180
Li.Liu <nox60@hotmail.com>
2019-11-18 22:33:56 +0800

    Update flyweight

--- design-pattern/flyweight2/flyweight.go
+++ design-pattern/flyweight2/flyweight.go
@@ -1,22 +1,35 @@
 package main
 
+import "fmt"
+
 type FlyWeight struct {
 	data string
 }
 
-var F map[string]*FlyWeight
+var F = make(map[string]*FlyWeight)
 
 func GetFLyWeight(fileName string) *FlyWeight {
 	image := F[fileName]
 	if image == nil {
-
+		image = &FlyWeight{data: fileName}
+		F[fileName] = image
 	}
-	/*
-		image := F[filename]
-		if image == nil {
-			image = NewImageFlyweight(filename)
-			f.maps[filename] = image
-		}
 
-	*/
+	return image
+}
+
+func main() {
+	fmt.Println(">>>>>>")
+	a := *GetFLyWeight("a11")
+	fmt.Println("a11.data: ", a.data, &a)
+
+	a12 := *GetFLyWeight("a12")
+	fmt.Println("a12.data: ", a12.data, &a12)
+
+	a13 := *GetFLyWeight("a13")
+	fmt.Println("a13.data: ", a13.data, &a13)
+
+	a14 := *GetFLyWeight("a11")
+	fmt.Println("a14.data: ", a14.data, &a14)
+
 }
