1d3e1ba47680c500571054101e88ad8b09dc399d
Li.Liu <nox60@hotmail.com>
2019-11-22 23:48:09 +0800

    add http downloader

--- design-pattern/templatemethod1/templateMain.go
+++ design-pattern/templatemethod1/templateMain.go
@@ -1,10 +1,16 @@
 package main
 
-import "daily_golang/design-pattern/templatemethod1/template"
+import (
+	"./template"
+	"fmt"
+)
 
 func main() {
 	ftpDownloader := template.FtpDownloader{}
 	template := template.Template{}
 	template.Implement = &ftpDownloader
 	template.Download("", "")
+
+	httpDownloader := template.HttpDownloader{}
+	fmt.Println(httpDownloader)
 }
